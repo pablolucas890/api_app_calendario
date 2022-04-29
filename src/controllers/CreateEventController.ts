a8c9537a5b3ddbf721cee692ab7033d5b6ffbcbe
Caioesqueletron <caiocaio620@gmail.com>
2022-04-29 10:41:03 -0300

    Criando evento

--- src/controllers/CreateEventController.ts
+++ src/controllers/CreateEventController.ts
@@ -1,8 +1,37 @@
+import { getRepository } from "typeorm"
+
+
+interface Request {
+    title:string,
+    description:string,
+    link:string,
+    image:Number,
+    eventType:Number,
+    calendarType:Number,
+    dateStart: Date,
+    dateEnd: Date
+
+}
 
 class CreateEventController{
 
-    public execute(){
+    public async execute({title, description, link, image, eventType, calendarType, dateStart, dateEnd}:Request): Promise<Event>{
+        const eventRepository = getRepository(Event)
+
+        const event = eventRepository.create({
+            title,
+            description,
+            link,
+            image,
+            eventType,
+            calendarType,
+            dateStart,
+            dateEnd
+        })
         
+        await eventRepository.save(event)
+
+        return event 
     }
 
 }
