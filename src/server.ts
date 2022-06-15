fc5909e2e2f0daab4b65e4e864278371a7d0e771
Caioesqueletron <caiocaio620@gmail.com>
2022-06-15 18:00:02 -0300

    Arrumando retorno Co-authored-by: Pablo <pablolucas890@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors'
 import express, { Request, Response, NextFunction } from 'express';
 
+
 import routes from './routes';
 import './database';
 import cors from 'cors';
